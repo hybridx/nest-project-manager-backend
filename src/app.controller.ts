2b95fc0e6ef451175b3286a64af66a281fe60863
hybridx <hybridx@gmail.com>
2022-12-15 19:21:11 +0530

    Session storage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/authenticated.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
@@ -22,6 +23,12 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
+    return req.user;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('protected')
+  protectedRoute(@Request() req): string {
     return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serilizer';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [UsersService, AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule.register({ session: true })],
+  providers: [UsersService, AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
