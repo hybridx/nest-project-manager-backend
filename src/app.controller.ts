7515fa561aeb24f6abb6dd487ff235a057f982c4
hybridx <hybridx@gmail.com>
2022-12-15 15:04:57 +0530

    passport-local added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @Header('Content-Type', 'text/html')
+  @Header('Content-Type', 'application/json')
   getHello(): any {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return req.user;
   }
 }
